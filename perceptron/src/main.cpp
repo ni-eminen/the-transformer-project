0c2e9caf89c9d7b434bcfd1640a5a5a9e31874a5
ni-eminen <matiasnie2@gmail.com>
2023-12-03 19:27:18 +0200

    restructuring code

--- perceptron/src/main.cpp
+++ perceptron/src/main.cpp
@@ -9,8 +9,8 @@ using namespace std;
 #include <algorithm>
 #include <numeric>
 #include <stdexcept>
-#include "utils.h"
 #include <cfloat>
+#include "utils.h"
 
 class Neuron {
   public:
@@ -52,54 +52,6 @@ class Neuron {
 
     double activation_function(double x) {
         return sigmoid(x);
-    }
-
-    std::vector<double> replaceZeros(std::vector<double> vec, double replacement) {
-        for(int i = 0; i<vec.size(); i++) {
-            if (vec[i] == 0.0) {
-                vec[i] = replacement;
-            }
-        }
-
-        return vec;
-    }
-
-    double binary_cross_entropy(std::vector<double> y, std::vector<double> y_pred) {
-        y = replaceZeros(y, DBL_MIN);
-        y_pred = replaceZeros(y_pred, DBL_MIN);
-
-        double result = 0;
-        for (int i = 0; i<y.size(); i++) {
-            if (y_pred[i] == 1) {
-                y_pred[i] = 1.0 - 1.0/pow(10, 10);
-            }
-
-            double temp = y[i] * log(y_pred[i]) + (1.0-y[i])*(log(1.0-y_pred[i]));
-            result = result + temp;
-        }
-
-        return -(1.0/y.size()) * result;
-    }
-
-
-
-    double d_binary_cross_entropy(std::vector<double> y, std::vector<double> y_pred) {
-        y = replaceZeros(y, DBL_MIN);
-        y_pred = replaceZeros(y_pred, DBL_MIN);
-
-
-
-        double result = 0;
-        for(int i = 0; i<y.size(); i++) {
-            if (y_pred[i] == 1) {
-                y_pred[i] = 1.0 - 1.0/pow(10, 10);
-            }
-
-            double temp = (y[i] / y_pred[i]) - ((1.0 - y[i]) / (1.0 - y_pred[i]));
-            result = result + temp;
-        }
-
-        return -(1.0/y.size()) * result;
     }
 
     double forward_propagate(std::vector<double> inputs) {
@@ -152,12 +104,10 @@ int main(int argc, char *argv[])
     // 1 0      0
     // 0 1      0
     // 0 0      0
-    Neuron perceptron = Neuron(1.0, .5, std::vector<double>{-1, 1 });
+    Neuron perceptron = Neuron(1.0, .5, std::vector<double>{-1, 1});
 
     std::vector<std::vector<double>> X = std::vector<std::vector<double>>{{1,1},    {1,0},  {0,1},  {0,0}};
     std::vector<std::vector<double>> y = std::vector<std::vector<double>>{{1},      {1},    {1},    {0}};
-
-    // std::vector<double> prev_w = perceptron.weights;
 
     // Training
     for(int i=0;i<10000;i++) {
@@ -169,9 +119,8 @@ int main(int argc, char *argv[])
         std::cout << "pred: " << round(pred) << std::endl;
     }
 
-    printVector(perceptron.weights);
+    printVector(perceptron.weights, "Weights after training");
     std::cout << perceptron.bias << std::endl;
-    // std::cout << perceptron.binary_cross_entropy(std::vector<double>{1,1,1}, std::vector<double>{1,1,0.000001}) << std::endl;
 
     return 0;
 }
