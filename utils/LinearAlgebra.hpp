d3331ec07d6d841be8e704de41ed3c329a7b6bac
ni-eminen <matiasnie2@gmail.com>
2024-01-06 00:30:41 +0200

    add elementWiseSum to header file

--- utils/LinearAlgebra.hpp
+++ utils/LinearAlgebra.hpp
@@ -9,5 +9,6 @@ vector<double> matMul(matrix A, std::vector<double> B);
 vector<double> matMul(std::vector<double> A, std::vector<double> B);
 
 matrix transpose(matrix A);
+vector<double> elementWiseSum(vector<double> a, vector<double> b);
 
 void printMatrix(matrix m, std::string label);
