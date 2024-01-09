7027aa65bbc2fd5ea385730011a425922ec45d18
ni-eminen <matiasnie2@gmail.com>
2024-01-09 15:59:30 +0200

    Initialize transformer files.

---
+++ transformer/src/main.cpp
@@ -0,0 +1,18 @@
+
+#include <vector>
+#include <iomanip>
+#include "utils.hpp"
+#include "Types.hpp"
+
+int main(int argc, char *argv[])
+{
+    int encoderHeadsN = 3;
+    int decoderHeadsN = 3;
+    double learningRate = .01;
+    Transformer transformer = Transformer();
+
+    vector<std::string> vocab = vector<std::string>();
+    vector<int> input = vector<int>(1, 0);
+
+    return 0;
+}
