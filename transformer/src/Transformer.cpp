ede77daa624def5ed6865945cb4f3bb02ef7acb5
ni-eminen <matiasnie2@gmail.com>
2024-01-24 19:45:05 +0200

    broken sdpa

--- transformer/src/Transformer.cpp
+++ transformer/src/Transformer.cpp
@@ -5,9 +5,11 @@ using namespace std;
 #include "LinearAlgebra.hpp"
 
 
-vector<double> scaledDotProductAttention(vector<double> K, vector<double> Q, vector<double> V) {
-  vector<vector<double>> QK = matmul
-  return softmax();
+vector<double> scaledDotProductAttention(vector<double> K, vector<double> Q, vector<double> V, double dim) {
+  vector<vector<double>> QK = matMul(Q, K);
+  vector<vector<double>> QK_t = transpose(QK);
+  vector<vector<double>> scaled_QK_t = scalarMultiplyMatrix((1/dim), QK_t);
+  return matMul(softmax(scaled_QK_t), V);
 }
 
 double multiHeadAttention(vector<double> K, vector<double> Q, vector<double> V) {
