32c785607bf55c53367ca505ebfb0b09873936b3
ni-eminen <matiasnie2@gmail.com>
2024-01-24 17:05:32 +0200

    stubs for Transformer functions

--- transformer/src/Transformer.cpp
+++ transformer/src/Transformer.cpp
@@ -2,20 +2,38 @@ using namespace std;
 #include <string>
 #include "Types.hpp"
 
-double scaledDotProductAttention(vector<double> K, vector<double> Q, vector<double> V);
+double scaledDotProductAttention(vector<double> K, vector<double> Q, vector<double> V) {
+  return 0.0;
+}
 
-double multiHeadAttention(vector<double> K, vector<double> Q, vector<double> V);
+double multiHeadAttention(vector<double> K, vector<double> Q, vector<double> V) {
+  return 0.0;
+}
 
-double maskedMultiHeadAttention(vector<double> K, vector<double> Q, vector<double> V);
+double maskedMultiHeadAttention(vector<double> K, vector<double> Q, vector<double> V) {
+  return 0.0;
+}
 
-vector<double> addAndNorm(vector<vector<double> > V);
+vector<double> addAndNorm(vector<vector<double> > V) {
+  return vector<double>();
+}
 
-vector<double> softmax(vector<double> v);
+vector<double> softmax(vector<double> v) {
+  return vector<double>();
+}
 
-vector<double> positionalEncoding(vector<double> v);
+vector<double> positionalEncoding(vector<double> v) {
+  return vector<double>();
+}
 
-vector<double> forward(vector<double> inputs);
+vector<double> forward(vector<double> inputs) {
+  return vector<double>();
+}
 
-void train(vector<double> x, vector<double> y);
+void train(vector<double> x, vector<double> y) {
+  return ;
+}
 
-double lossFunction(vector<double> y, vector<double> y_pred);
+double lossFunction(vector<double> y, vector<double> y_pred) {
+  return ;
+}
